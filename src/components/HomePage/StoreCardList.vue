89ea69fb1f76ae7ea75854878ee8b4ce84cee4da
Chouwill <will92068@gmail.com>
2026-01-06 19:02:47 +0800

    feat(home): add store card list component and update homepage icons

---
+++ src/components/HomePage/StoreCardList.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="bg-red-300 flex justify-evenly  gap-5">
+    <div class="card card-side bg-base-100 shadow-sm w-[400px] h-[280px]">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          alt="Movie"
+          class=""
+        />
+      </figure>
+      <div class="card-body border flex gap-3 pt-5 pr-3 pl-3">
+        <h2 class="card-title border">Cafe Jia Song 咖央</h2>
+
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-location.png" alt="" />
+          <p>台北市大安區大安路一段51巷2號2樓</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-time.png" alt="" />
+          <p>08:00 - 19:00</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-stairs.png" alt="" />
+          <p>3</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-subway.png" alt="" />
+          <p>忠孝復興捷運站</p>
+        </div>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+    <div class="card card-side bg-base-100 shadow-sm w-[400px] h-[280px]">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          alt="Movie"
+          class=""
+        />
+      </figure>
+      <div class="card-body border flex gap-3 pt-5 pr-3 pl-3">
+        <h2 class="card-title border">Cafe Jia Song 咖央</h2>
+
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-location.png" alt="" />
+          <p>台北市大安區大安路一段51巷2號2樓</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-time.png" alt="" />
+          <p>08:00 - 19:00</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-stairs.png" alt="" />
+          <p>3</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-subway.png" alt="" />
+          <p>忠孝復興捷運站</p>
+        </div>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+    <div class="card card-side bg-base-100 shadow-sm w-[400px] h-[280px]">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          alt="Movie"
+          class=""
+        />
+      </figure>
+      <div class="card-body border flex gap-3 pt-5 pr-3 pl-3">
+        <h2 class="card-title border">Cafe Jia Song 咖央</h2>
+
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-location.png" alt="" />
+          <p>台北市大安區大安路一段51巷2號2樓</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-time.png" alt="" />
+          <p>08:00 - 19:00</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-stairs.png" alt="" />
+          <p>3</p>
+        </div>
+        <div class="w-[150px] flex gap-1 items-center">
+          <img class="w-[25px] h-[25px]" src="@/image/icon-subway.png" alt="" />
+          <p>忠孝復興捷運站</p>
+        </div>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HomeContent from '@/components/HomePage/HomeContent.vue'
 import HomeFilterForm from '@/components/HomePage/HomeFilterForm.vue'
+import StoreCardList from '@/components/HomePage/StoreCardList.vue';
 </script>
 
 <template>
   <div class="contciner">
     <HomeContent />
     <HomeFilterForm />
+    <StoreCardList />
   </div>
 </template>
